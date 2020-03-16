84697ed1158320101f28cb73264a4985d9d4f4a3
Parth Panchal <pr930523@dal.ca>
2020-03-16 20:30:43 -0300

    "data feeds for home page is configured"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserFeeds} from '../homedatafeeds';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  UserFeeds = UserFeeds;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/homedatafeeds.ts
@@ -0,0 +1,17 @@
+export const UserFeeds = [
+  {
+    title: 'Hydro energy to run vehicles',
+    likes: 450,
+    description: 'The project has been proposed by grad alumni of MIT to enhance the utilization of Hydrocycle hypothesis'
+  },
+  {
+    name: 'ML for agriculture',
+    likes: 275,
+    description: 'The combine initiative of former employees from L&T and grad student of Dalhousie MACS'
+  },
+  {
+    name: 'Precise Robotics',
+    likes: 299,
+    description: 'Joint initiative from UoT and Dalhousie student to resolve the pattern recognition while undergoing path optimization'
+  }
+];
