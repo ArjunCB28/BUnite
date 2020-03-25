a0d9305e937c530064f64d51a4aeb1c7df983fb2
Mahavir Vataliya <mahavir@infobizzs.com>
2020-03-25 18:39:09 +0530

    Updated homefeed as per display data

--- src/app/homedatafeeds.ts
+++ src/app/homedatafeeds.ts
@@ -1,17 +1,30 @@
 export const UserFeeds = [
   {
-    title: 'Hydro energy to run vehicles',
+    name: 'Hydro energy to run vehicles',
     likes: 450,
-    description: 'The project has been proposed by grad alumni of MIT to enhance the utilization of Hydrocycle hypothesis'
+    description: 'The project has been proposed by grad alumni of MIT to enhance the utilization of Hydrocycle hypothesis',
+    avatar: 'https://www.real-world-physics-problems.com/images/xsalt_water_fuel_cell_car.jpg.pagespeed.ic.ihBGX2kkja.jpg',
+    liked: true,
   },
   {
     name: 'ML for agriculture',
     likes: 275,
-    description: 'The combine initiative of former employees from L&T and grad student of Dalhousie MACS'
+    description: 'The combine initiative of former employees from L&T and grad student of Dalhousie MACS',
+    avatar: 'https://mk0egoveletsonla87wv.kinstacdn.com/wp-content/uploads/2019/10/AI-ML-and-IoT-are-Deployed-in-Agriculture-to-Optimise-Crop-Yield.jpg',
+    liked: true,
   },
   {
     name: 'Precise Robotics',
     likes: 299,
-    description: 'Joint initiative from UoT and Dalhousie student to resolve the pattern recognition while undergoing path optimization'
+    description: 'Joint initiative from UoT and Dalhousie student to resolve the pattern recognition while undergoing path optimization',
+    avatar: 'https://coro.etsmtl.ca/blog/images/PreciseFlex400.jpg',
+    liked: true,
+  },
+  {
+    name: 'Transform the way you work',
+    likes: 299,
+    description: 'Joint initiative from UoT and Dalhousie student to resolve the pattern recognition while undergoing path optimization',
+    avatar: 'https://cdn.wedevs.com/uploads/2018/08/Business-Operations-Manager-for-WordPress-750x300.png',
+    liked: true,
   }
 ];
