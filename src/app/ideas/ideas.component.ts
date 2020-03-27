4edfbc8d1f238d11abdd979161eb6e6194b7d685
Kushal Reshamdalal <kushalreshamdalal@Kushals-MacBook-Pro.local>
2020-03-27 20:20:29 -0300

    minor changes

--- src/app/ideas/ideas.component.ts
+++ src/app/ideas/ideas.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ideas',
   templateUrl: './ideas.component.html',
-  styleUrls: ['./ideas.component.css']
+  styleUrls: ['./ideas.component.less']
 })
 export class IdeasComponent implements OnInit {
 
