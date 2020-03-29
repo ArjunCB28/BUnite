8a2377f1c84fb4e6f37891561bb5c172dc400dd7
Pranay Rajesh Gheewala <pranaygheewala1996@gmail.com>
2020-03-28 21:45:04 -0300

    Initial commit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.less']
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
